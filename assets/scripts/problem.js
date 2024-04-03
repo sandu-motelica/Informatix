0c34e6acb3ab7dbe2b95accce89747563a0b936d
Florin <48189025+FlorinInfo@users.noreply.github.com>
2024-04-03 21:26:32 +0300

    sync

---
+++ assets/scripts/problem.js
@@ -0,0 +1,8 @@
+import { rootPath } from "./constants.js";
+const searchParams = new URLSearchParams(window.location.search);
+const problemHeadline = document.getElementsByClassName('problem__title')[0];
+
+if(searchParams.has('id')) {
+    problemHeadline.append(searchParams.get('id'))
+}
+else window.location.href = `${rootPath}/problems.html`;
