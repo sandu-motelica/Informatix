2938b1d80e218c0d095e09abdb3e07dc9386cf54
sandu-motelica <motelica.sb@gmail.com>
2024-04-07 01:32:19 +0300

    edit problem page

--- assets/scripts/problem.js
+++ assets/scripts/problem.js
@@ -1,8 +1,7 @@
 import { rootPath } from "./constants.js";
 const searchParams = new URLSearchParams(window.location.search);
-const problemHeadline = document.getElementsByClassName('problem__title')[0];
+const problemHeadline = document.getElementsByClassName("problem__title")[0];
 
-if(searchParams.has('id')) {
-    problemHeadline.append(searchParams.get('id'))
-}
-else window.location.href = `${rootPath}/problems.html`;
+if (searchParams.has("id")) {
+  // problemHeadline.append(searchParams.get('id'))
+} else window.location.href = `${rootPath}/problems.html`;
