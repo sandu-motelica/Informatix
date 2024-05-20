80a9b3fec329eaf13552bab0f2e7d006c788792a
sandu-motelica <motelica.sb@gmail.com>
2024-05-20 12:48:39 +0300

    add get solution with some difficulty

--- BACKEND/controllers/SolutionController.js
+++ BACKEND/controllers/SolutionController.js
@@ -1,4 +1,5 @@
 import { Solution } from "../models/solutionModel.js";
+import { Problem } from "../models/problemModel.js";
 import { User } from "../models/userModel.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import errorMiddleware from "../middlewares/errorMiddleware.js";
@@ -37,6 +38,28 @@ export const addSolution = async (req, res) => {
 
     res.statusCode = 201;
     res.end(JSON.stringify(new SolutionDto(solution)));
+  } catch (e) {
+    errorMiddleware(res, e);
+  }
+};
+export const getSolutionsWithDiff = async (req, res) => {
+  try {
+    await authMiddleware(req, res);
+    const body = JSON.parse(req.data);
+    const { difficulty } = body;
+    const userId = req.user.payload.id;
+
+    const problems = await Problem.find({ difficulty }).lean();
+
+    const problemIds = problems.map((problem) => problem._id);
+
+    const solutions = await Solution.find({
+      id_student: userId,
+      id_problem: { $in: problemIds },
+    }).lean();
+
+    res.statusCode = 200;
+    res.end(JSON.stringify({ count: solutions.length }));
   } catch (e) {
     errorMiddleware(res, e);
   }
