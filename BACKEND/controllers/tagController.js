5eaeca6708ca94da44d885273fa273670fd61fdf
Florin <florinainol@gmail.com>
2024-06-22 09:10:26 +0300

    fix tag section on add problem page

--- BACKEND/controllers/tagController.js
+++ BACKEND/controllers/tagController.js
@@ -6,7 +6,16 @@ import errorMiddleware from "../middlewares/errorMiddleware.js";
 export const getTags = async (req, res) => {
   try {
     // await authMiddleware(req, res);
-    const tags = await Tag.find({});
+    const tags = await Tag.find({}).lean();
+    const tagIds = tags.map((tag) => tag._id);
+    const counts = await ProblemTags.aggregate([
+      { $match: { id_tag: { $in: tagIds } } },
+      { $group: { _id: "$id_tag", count: { $sum: 1 } } },
+    ]);
+
+    tags.forEach((tag, index) => (tag.count = counts[index].count));
+
+    console.log(tags);
     res.statusCode = 200;
     res.end(JSON.stringify(tags));
   } catch (e) {
