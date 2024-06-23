4ea60d194e28802edbad60846f1892813b40ed74
Florin <florinainol@gmail.com>
2024-06-23 13:47:46 +0300

    add user stats + solutions on profile page

--- FRONTEND/assets/scripts/account.js
+++ FRONTEND/assets/scripts/account.js
@@ -1,5 +1,6 @@
 import unauthorizedRedirect from "./unauthorized.js";
 import Fetch from "../../utils/Fetch.js";
+import { rootPath } from "./constants.js";
 unauthorizedRedirect();
 
 const searchParams = new URLSearchParams(window.location.search);
@@ -9,20 +10,9 @@ const getUserInfo = async () => {
   const data = await Fetch.get("/user/info");
   if (data?.statusCode === 200) {
     try {
-      const { user, solutions } = data;
+      const { user } = data;
       const name = document.querySelector(".account__info__name");
       name.textContent = user.username;
-      const solutionNumber = document.querySelectorAll(
-        ".account__statistic__solutions"
-      );
-      for (let i = 0; i < solutionNumber.length; i++) {
-        let diff;
-        if (i === 0) diff = "easy";
-        else if (i === 1) diff = "medium";
-        else diff = "hard";
-        const count = solutions.filter((sol) => sol.difficulty === diff).length;
-        solutionNumber[i].textContent = count;
-      }
     } catch (e) {
       console.log(e);
     }
@@ -31,19 +21,79 @@ const getUserInfo = async () => {
 
 getUserInfo();
 
-const getSolution = async () => {
-  const data = await Fetch.get("/problem/difficulty");
-  if (data?.statusCode === 200) {
+const getSolutions = async () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const data = await Fetch.get("/solution", {
+    id_student: user.id,
+  });
+
+  const solutionsBlock = document.querySelector(
+    ".account__stats-solved-problems"
+  );
+  data.solutions.forEach((item) => {
+    const dateStr = item?.created_time;
+    const date = new Date(dateStr);
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const year = date.getFullYear();
+
+    const solvedProblems = data.solutions.filter((item) => item.grade > 8);
+    const easyProblems = solvedProblems.filter(
+      (item) => item.id_problem?.difficulty === "easy"
+    );
+    document.querySelector(
+      ".account__statistic--easy .account__statistic__solutions"
+    ).innerHTML = easyProblems.length;
+    const mediumProblems = solvedProblems.filter(
+      (item) => item.id_problem?.difficulty === "medium"
+    );
+    document.querySelector(
+      ".account__statistic--medium .account__statistic__solutions"
+    ).innerHTML = mediumProblems.length;
+    const hardProblems = solvedProblems.filter(
+      (item) => item.id_problem?.difficulty === "hard"
+    );
+    document.querySelector(
+      ".account__statistic--hard .account__statistic__solutions"
+    ).innerHTML = hardProblems.length;
     console.log(data);
-    try {
-      // const { count } = data;
-      // const name = document.querySelector(".account__info__name");
-      // console.log(name);
-      // name.textContent = username;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+
+    let a = document.createElement("a");
+    a.href = `${rootPath}/problem.html?id=${item.id_problem.id}&solution=${item.id}`;
+    a.classList.add("account__stats__problem");
+    a.innerHTML = `${item.id_problem.title} - (${
+      item?.grade > 0 ? item?.grade : "Neevaluata"
+    }) <span>${day}/${month}/${year}</span>`;
+    solutionsBlock.appendChild(a);
+  });
 };
 
-getSolution();
+getSolutions();
+
+const getProblems = async () => {
+  const data = await Fetch.get("/problem", {
+    status: "approved",
+  });
+
+  const easyProblems = data?.problems.filter(
+    (item) => item?.difficulty === "easy"
+  );
+  document.querySelector(
+    ".account__statistic--easy .account__statistic__problems"
+  ).innerHTML = easyProblems.length;
+  const mediumProblems = data?.problems.filter(
+    (item) => item?.difficulty === "medium"
+  );
+  document.querySelector(
+    ".account__statistic--medium .account__statistic__problems"
+  ).innerHTML = mediumProblems.length;
+  const hardProblems = data?.problems.filter(
+    (item) => item?.difficulty === "hard"
+  );
+  document.querySelector(
+    ".account__statistic--hard .account__statistic__problems"
+  ).innerHTML = hardProblems.length;
+  console.log(easyProblems);
+};
+
+getProblems();
