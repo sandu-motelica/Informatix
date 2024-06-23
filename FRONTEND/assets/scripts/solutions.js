a545f19505dbac5d996cebd33cebeae5fc2f7805
Florin <florinainol@gmail.com>
2024-06-23 09:11:08 +0300

    remove solutions from homework from solutions page

--- FRONTEND/assets/scripts/solutions.js
+++ FRONTEND/assets/scripts/solutions.js
@@ -9,29 +9,31 @@ const getSolutions = async () => {
   });
 
   console.log(data);
-  data.solutions.forEach((solution) => {
-    let tr = document.createElement("tr");
-    tr.setAttribute("data-id", solution?.id);
+  data.solutions
+    .filter((item) => !item.id_homework)
+    .forEach((solution) => {
+      let tr = document.createElement("tr");
+      tr.setAttribute("data-id", solution?.id);
 
-    const dateStr = solution?.created_time;
-    const date = new Date(dateStr);
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const year = date.getFullYear();
+      const dateStr = solution?.created_time;
+      const date = new Date(dateStr);
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+      const year = date.getFullYear();
 
-    const problem = solution.id_problem;
-    const author = solution.id_student;
+      const problem = solution.id_problem;
+      const author = solution.id_student;
 
-    tr.innerHTML = `
+      tr.innerHTML = `
           <th>${day}/${month}/${year}</th>
           <th>${author?.username}</th>
           <th>${solution?.grade != 0 ? solution?.grade : "Neevaluata"}</th>
           <th><a href='${rootPath}/problem.html?id=${problemId}&solution=${
-      solution.id
-    }'>Verifica solutie</a></th>
+        solution.id
+      }'>Verifica solutie</a></th>
         `;
-    document.getElementById("solutions-list").appendChild(tr);
-  });
+      document.getElementById("solutions-list").appendChild(tr);
+    });
 };
 
 getSolutions();
